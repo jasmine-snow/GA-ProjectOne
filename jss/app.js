45c88a23119940a4de58cb0af09fe34bc935e9f1
Jasmine Snow <jasminesimmone@gmail.com>
2020-10-06 17:29:13 -0700

    random function is working

--- jss/app.js
+++ jss/app.js
@@ -1,6 +1,6 @@
 $(()=>{
 
-  const gameBoard = (event) => {
+
 
 
     /*===============
@@ -8,55 +8,116 @@ $(()=>{
     peach class. So I can use that to flip the cards!!
     // =================*/
     //   $(event.target).addClass('peach')
-  }
+  // }
   const animalsArray = [
-    {animal: 'Elephant', image: './img-project/elephant.jpg'},
-    {animal: 'Iguana', image: './img-project/iguana.jpg' },
-    {animal: 'Lion', image: './img-project/lion.jpg'},
-    {animal: 'Panda', image: './img-project/panda.jpg'},
-    {animal: 'Penguin', image: './img-project/penguin.jpg'},
-    {animal: 'Pig', image: './img-project/pig.jpg'},
-    {animal: 'Sheep', image: './img-project/sheep.jpg'},
-    {animal: 'Zebra', image: './img-project/zebra.jpg'},
-    {animal: 'Dog', image: './img-project/dog.jpg'},
-    {animal: 'Hippo', image: './img-project/hippo.jpg'},
-    {animal: 'Fox', image: './img-project/fox.jpg'},
-    {animal: 'Owl', image: './img-project/owl.jpg'},
-    {animal: 'Cat', image: './img-project/cat.jpg'},
-    {animal: 'Giraffe', image: './img-project/giraffe.jpg'},
-    {animal: 'Squirrel', image: './img-project/squirrel.jpg'},
-    {animal: 'Donky', image: './img-project/donky.jpg'},
-    {animal: 'Tiger', image: './img-project/tiger.jpg'},
-    {animal: 'WILD CARD', image: './img-project/wildCard.jpg'},
-    {animal: 'Horse', image: './img-project/horse.jpg'},
-    {animal: 'Octopus', image: './img-project/octopus.jpg'},
-    {animal: 'Turtle', image: './img-project/turtle.jpg'},
-    {animal: 'Snake', image: './img-project/snake.jpg'},
-    {animal: 'Whale', image: './img-project/whale.jpg'},
-    {animal: 'Beaver', image: './img-project/beaver.jpg'},
-    {animal: 'Mouse', image: './img-project/Mouse.jpg'},
-    {animal: 'Chicken', image: './img-project/chicken.jpg'},
-    {animal: 'Frog', image: './img-project/frog.jpg'},
-    {animal: 'Cow', image: './img-project/cow.jpg'},
-    {animal: 'Seal', image: './img-project/seal.jpg'},
-    {animal: 'Goat', image: './img-project/goat.jpg'}
+    {animal: 'Elephant', image: './img-project/elephant.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Iguana', image: './img-project/iguana.jpg', background:'./img-project/front-card.jpg' },
+    {animal: 'Lion', image: './img-project/lion.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Panda', image: './img-project/panda.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Penguin', image: './img-project/penguin.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Pig', image: './img-project/pig.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Sheep', image: './img-project/sheep.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Zebra', image: './img-project/zebra.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Dog', image: './img-project/dog.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Hippo', image: './img-project/hippo.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Fox', image: './img-project/fox.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Owl', image: './img-project/owl.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Cat', image: './img-project/cat.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Giraffe', image: './img-project/giraffe.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Squirrel', image: './img-project/squirrel.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Donky', image: './img-project/donky.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Tiger', image: './img-project/tiger.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'WILD CARD', image: './img-project/wildCard.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Horse', image: './img-project/horse.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Octopus', image: './img-project/octopus.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Turtle', image: './img-project/turtle.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Snake', image: './img-project/snake.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Whale', image: './img-project/whale.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Beaver', image: './img-project/beaver.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Mouse', image: './img-project/Mouse.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Chicken', image: './img-project/chicken.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Frog', image: './img-project/frog.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Cow', image: './img-project/cow.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Seal', image: './img-project/seal.jpg', background:'./img-project/front-card.jpg'},
+    {animal: 'Goat', image: './img-project/goat.jpg', background:'./img-project/front-card.jpg'}
 
   ];
 
 
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < animalsArray.length; i++) {
     // let $div = $('<div>')
-    let $listName = $(`<li><img src="${animalsArray[i].image}" class="animalCard"/></li>`)
-    let $listWord = $(`<li class="animalBackCard" id="${animalsArray[i].animal}"> </li>`)
+    let $showBackground = $(`<div class="background-img"><img id="two" src="${animalsArray[i].background}" class="animalBackCard"/></div>`)
 
-    $listName.addClass('listName')
-    $('.img-container').append($listName)
+    let $showImage = $(`<div class="images"><img id="one" src="${animalsArray[i].image}" class="animalCard"/></div>`)
 
-    $listWord.addClass('listWord')
-    $('.img-container').append($listWord)
+    $showBackground.addClass('showBackground')
+    $('.flip-card-front').append($showBackground)
 
-  }
+    $showImage.addClass('showImage')
+    $('.flip-card-back').append($showImage)
 
+ }
+ const chooseRandom = () => {
+   let randomIndex = Math.floor(Math.random() * animalsArray.length)
+      return animalsArray[randomIndex]
+     //Math.floor(Math.Random() * animalsArray[i].image.length )
+     console.log(randomIndex)
+
+}
+    // console.log(chooseRandom().image)
+
+
+ const $card = $('.animalBackCard').on('click', (event) => {
+   console.log($(event.currentTarget).attr('src'))
+   $(event.currentTarget).replaceWith(chooseRandom().image)
+   console.log('hello')
+ })
+
+
+
+
+
+
+
+///find out if this is correct and add toggle
+/// $(event.currentTarget)
+//replaceWith()
+//.attr('src', "")
+//.replaceWith($('.animalBackCard'))
+//remove()   // .replaceWith($('.animalBackCard'))
+
+//before($('.animalBackCard').hide());
+
+
+
+
+
+
+
+
+
+
+//
+//  for (let i = 0; i < 30; i++) {
+//    // let $div = $('<div>')
+//    let $listName = $(`<li><img src="${animalsArray[i].image}" class="animalCard"/></li>`)
+//    let $listWord = $(`<li class="animalBackCard"> ${animalsArray[i].animal} </li>`)
+//
+//    $listName.addClass('listName')
+//    $('.img-container').append($listName)
+//
+//    $listWord.addClass('listWord')
+//    $('.word-container').append($listWord)
+//
+//
+//
+// }
+ //
+ // $listName.addClass('listName')
+ // $('.img-container').append($listName)
+ //
+ // $listWord.addClass('listWord')
+ // $('.img-container').append($listWord)
   //
   // $('.animalCard').click(function() {
   //   $('.animalBackCard').toggle('.animalCard');
@@ -97,7 +158,7 @@ $(()=>{
   //  }
 
 
-  $('div').on('click', gameBoard)
+  // $('div').on('click', gameBoard)
 
 });
 
